3dcabb60a0a839074430e9c0bc0d5436b9e29968
vitalii boiko <vukelan@gmail.com>
2024-11-03 23:10:12 +0200

    cleanup code

--- src/Home.vue
+++ src/Home.vue
@@ -1,13 +1,8 @@
 <script setup>
 import HomeTitle from './home/HomeTitle.vue';
 import * as css from './home/home.module.scss';
-console.log(css);
 </script>
 
 <template>
   <div :class="css.homeDivOuter"><HomeTitle /></div>
 </template>
-
-<!-- <style lang="scss" module="css">
-@use './home/home.scss';
-</style> -->
