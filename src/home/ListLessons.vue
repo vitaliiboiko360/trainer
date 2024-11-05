b1db0e3384c2f217eb86b18727db0d39d4ab6f10
vitalii boiko <vukelan@gmail.com>
2024-11-05 23:06:48 +0200

    add lesson item wrap

--- src/home/ListLessons.vue
+++ src/home/ListLessons.vue
@@ -1,4 +1,5 @@
 <script setup>
+import * as css from './home.module.scss';
 import { useQuery } from '@tanstack/vue-query';
 
 const { isPending, isError, data, error } = useQuery({
@@ -23,6 +24,8 @@ const getKey = () => {
   <span v-else-if="isError">Error: {{ error.message }}</span>
   <!-- We can assume by this point that `isSuccess === true` -->
   <div v-else>
-    <p v-for="item in data.texts" :key="getKey">{{ item.title }}</p>
+    <div v-for="item in data.texts" :key="getKey" :class="css.lessonListItem">
+      <p>{{ item.title }}</p>
+    </div>
   </div>
 </template>
