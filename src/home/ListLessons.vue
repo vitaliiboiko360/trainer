e13dd9b9668de88418f793bbd6acd59171337c86
vitalii boiko <vukelan@gmail.com>
2024-11-06 22:08:08 +0200

    add todo

--- src/home/ListLessons.vue
+++ src/home/ListLessons.vue
@@ -2,9 +2,12 @@
 import * as css from './home.module.scss';
 import { useQuery } from '@tanstack/vue-query';
 
+import LessonListItem from './LessonListItem.vue';
+
 const { isPending, isError, data, error } = useQuery({
   queryKey: ['list'],
   queryFn: async () => {
+    // TODO: make it default query function takes url as parameter
     const response = await fetch('/data/list_of_texts.json');
     if (!response.ok) {
       throw new Error('Network response was not ok');
