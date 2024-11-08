5f8371dcf952ee64b37c1342b9522decfd99f431
vitalii boiko <vukelan@gmail.com>
2024-11-08 13:23:35 +0200

    refactor query json function

--- src/query/default.ts
+++ src/query/default.ts
@@ -1,7 +1,11 @@
-export async function queryJson(url) {
+const getResponse = async (url) => {
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
-  return response.json();
+  return response;
+};
+
+export async function queryJson(url) {
+  return (await getResponse(url)).json();
 }
