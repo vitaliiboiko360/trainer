aa990941384ee2a23f4dde9f1921ff59da322016
vitalii boiko <vukelan@gmail.com>
2024-11-08 15:38:25 +0200

    add event bus

--- src/page/LineSentence.vue
+++ src/page/LineSentence.vue
@@ -1,11 +1,22 @@
 <script setup>
+const emit = defineEmits(['clickTextSentenceEvent']);
 import * as css from './page.module.scss';
 const { textLine: textLineInfo } = defineProps(['textLine']);
-const { text: textLine, endParagraph = false } = textLineInfo;
+const {
+  text: textLine,
+  endParagraph = false,
+  start: startTime,
+  end: endTime,
+} = textLineInfo;
+const onClick = (event) => {
+  console.log(`you've clicked on`, event.currentTarget);
+  emit('clickTextSentenceEvent', { startTime: startTime, endTime: endTime });
+};
 </script>
 
 <template>
-  <span :class="endParagraph ? css.lineUnderlined : ''"> {{ textLine }} </span
+  <span @click="onClick" :class="endParagraph ? css.lineUnderlined : ''">
+    {{ textLine }} </span
   >&nbsp;
   <br v-if="endParagraph" />
 </template>
--- src/page/audio/Audio.vue
+++ src/page/audio/Audio.vue
@@ -1,7 +1,45 @@
 <script setup>
+import { useTemplateRef, ref, watch, watchEffect, onMounted } from 'vue';
+const audio = useTemplateRef('audioTrack');
 const { audioSource } = defineProps(['audioSource']);
+
+const previousOnTimeUpdateHandler = ref();
+
+watchEffect(() => {
+  if (!audio.value) return;
+  const onClickTextSentenceEvent = audio.value
+    ? (eventData) => {
+        console.log(`eventData=`, eventData);
+        const { startTime, endTime } = eventData;
+        audio.value.currentTime = startTime;
+        audio.value.play();
+        if (previousOnTimeUpdateHandler.value)
+          audio.value.removeEventListner(
+            'timeupdate',
+            previousOnTimeUpdateHandler.value
+          );
+        previousOnTimeUpdateHandler.value = audio.value.addEventListener(
+          'timeupdate',
+          (event) => {
+            if (audio.value.currentTime >= endTime) audio.value.pause();
+          }
+        );
+      }
+    : () => {
+        console.log(`empty audio`);
+      };
+
+  audio.value.addEventListener(
+    'clickTextSentenceEvent',
+    onClickTextSentenceEvent
+  );
+});
 </script>
 
 <template>
-  <audio :src="`data/${audioSource}`"></audio>
+  <audio
+    @clickTextSentenceEvent="onClickTextSentenceEvent"
+    ref="audioTrack"
+    :src="`data/${audioSource}`"
+  ></audio>
 </template>
