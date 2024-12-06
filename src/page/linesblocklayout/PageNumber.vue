f7bb373e6b3da219a9e0cb1a14b2d4110ac8cb8b
vitaliiboiko360 <vukelan@gmail.com>
2024-12-06 23:38:56 +0200

    add onClick on goto page numbers

--- src/page/linesblocklayout/PageNumber.vue
+++ src/page/linesblocklayout/PageNumber.vue
@@ -2,15 +2,22 @@
 import css from './linesblocklayout.module.scss';
 import { defineProps } from 'vue';
 const { index, isActive } = defineProps(['index', 'isActive']);
+import { currentPageBlock } from '../state/currentPageBlock';
+
+const onClick = (setNewIndex) => {
+  currentPageBlock.value = setNewIndex;
+};
 </script>
 
 <template>
   <p
     v-if="index == 1"
     :class="[css.pTagPageNumber_1, isActive && css.pTagPageNumberSelected]"
+    @click="() => onClick(index - 1)"
   ></p>
   <p
     v-if="index == 2"
     :class="[css.pTagPageNumber_2, isActive && css.pTagPageNumberSelected]"
+    @click="() => onClick(index - 1)"
   ></p>
 </template>
