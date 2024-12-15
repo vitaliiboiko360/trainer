6e1c8c19e78004be6beb8f2d6df112d3e2eb710b
vitalii boiko <vukelan@gmail.com>
2024-12-15 21:58:15 +0200

    position text block

--- src/home/details/TextBlock.vue
+++ src/home/details/TextBlock.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { CENTER_X, CENTER_Y } from './etc';
 import * as css from '../home.module.scss';
 </script>
 
-<template><div :class="css.textBlockOuterDiv"></div></template>
+<template>
+  <div :class="[css.textBlockOuterDiv, $style.localTextBlock]"></div>
+</template>
+
+<style module>
+.localTextBlock {
+  left: calc(v-bind('CENTER_X') * 1rem);
+  top: calc(v-bind('CENTER_Y') * 1rem);
+}
+</style>
