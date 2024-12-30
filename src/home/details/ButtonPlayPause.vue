9ee9bd243c95e47e4ac7a5d0bee9a26c84ff5697
vitalii boiko <vukelan@gmail.com>
2024-12-30 22:40:00 +0200

    leftovers

--- src/home/details/ButtonPlayPause.vue
+++ src/home/details/ButtonPlayPause.vue
@@ -1,9 +1,18 @@
 <script setup>
 import * as css from '../home.module.scss';
+import { textBlock } from './refs';
+
+const onClick = (event) => {
+  if (event.detail == 2) {
+    textBlock.value = 1;
+  } else {
+    textBlock.value = 0;
+  }
+};
 </script>
 
 <template>
   <div :class="css.buttonHolderDiv">
-    <button :class="css.buttonPlayPause"></button>
+    <button :class="css.buttonPlayPause" @click="onClick"></button>
   </div>
 </template>
--- src/home/details/TextBlock.vue
+++ src/home/details/TextBlock.vue
@@ -1,10 +1,64 @@
 <script setup>
+import { gsap } from 'gsap';
+import { watch, ref, useTemplateRef } from 'vue';
 import { CENTER_X, CENTER_Y } from './etc';
 import * as css from '../home.module.scss';
+import { textBlock } from './refs';
+
+const divElement = ref();
+
+let paramsInit = {
+  height: 0,
+  width: 0,
+  left: CENTER_X,
+  top: CENTER_Y,
+};
+
+let paramsTo = {
+  height: 5,
+  width: 20,
+  left: -10,
+  top: -5,
+};
+
+let isPrinted;
+const onDrawUpdate = (target) => {
+  if (!isPrinted) {
+    isPrinted = true;
+    console.log(`target=${target}`);
+  }
+  // if (!target) {
+  //   console.log('onDrawUpdate target = null');
+  //   return;
+  // }
+  target.height &&= `${paramsInit.height}rem`;
+  target.width &&= `${paramsInit.width}rem`;
+};
+
+const startAnimation = (target) => {
+  gsap.to(paramsInit, {
+    duration: 1.5,
+    height: paramsTo.height,
+    width: paramsTo.width,
+    // left: paramsTo.left,
+    // top: paramsTo.top,
+    onUpdate: () => onDrawUpdate(target),
+  });
+};
+
+watch(textBlock, () => {
+  console.log(`==${textBlock.value}`);
+  if (textBlock.value != 1) return;
+  startAnimation(divElement);
+  console.log(`divElement.value=${divElement?.value}`);
+});
 </script>
 
 <template>
-  <div :class="[css.textBlockOuterDiv, $style.localTextBlock]"></div>
+  <div
+    :ref="divElement"
+    :class="[css.textBlockOuterDiv, $style.localTextBlock]"
+  ></div>
 </template>
 
 <style module>
