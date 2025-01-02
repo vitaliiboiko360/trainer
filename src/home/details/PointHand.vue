26dbee3fa1046066a9564409fd674a79751584fe
vitalii boiko <vukelan@gmail.com>
2025-01-02 18:52:41 +0200

    add point hand

--- src/home/details/PointHand.vue
+++ src/home/details/PointHand.vue
@@ -1,14 +1,34 @@
 <script setup>
-import { IMG_POINT_HAND_SRC } from 'etc.ts';
+import { onMounted, ref, watch } from 'vue';
+import { IMG_POINT_HAND_SRC } from './etc';
 import { gsap } from 'gsap';
+import { pointHand } from './refs';
+
+const refImg = ref();
+onMounted(() => {
+  gsap.set(refImg.value, {
+    opacity: 0,
+  });
+});
+watch(pointHand, () => {
+  if (pointHand.value != 1) return;
+  console.log(`pointHand= ${pointHand}`);
+});
 </script>
 
 <template>
-  <img src="`${IMG_POINT_HAND_SRC}`" />
+  <img :ref="refImg" :class="[$style.imgHand]" :src="`${IMG_POINT_HAND_SRC}`" />
 </template>
 
 <style module>
 .displayNone {
   display: none;
+}
+.imgHand {
+  height: 3rem;
+  width: 3rem;
+  position: absolute;
+  top: 1rem;
+  left: 5rem;
 }
 </style>
