ad23740d8ff98b83959d4b3e1296a6dc03ce2b2a
vitalii boiko <vukelan@gmail.com>
2025-01-04 00:20:56 +0200

    add listenting head to animation

---
+++ src/home/details/ListenginHead.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { SVG_SRC, CENTER_X_REM, CENTER_Y_REM, SVG_DIMS } from './etc';
+import Svg from '../Svg.vue';
+
+const PROPS = {
+  top: '-1.5rem',
+  left: '23rem',
+  width: '13rem',
+  height: '14rem',
+};
+const xmlns = 'http://www.w3.org/1999/xhtml';
+</script>
+
+<template>
+  <Svg :class="[$style.imgSvg]">
+    <foreignObject :xmlns :height="PROPS.height" :width="PROPS.width">
+      <object :data="SVG_SRC" :type="`image/svg+xml`"></object>
+    </foreignObject>
+  </Svg>
+</template>
+
+<style module>
+.imgSvg {
+  position: absolute;
+  opacity: 0.1;
+  width: v-bind(PROPS.width);
+  height: v-bind(PROPS.height);
+  top: v-bind(PROPS.top);
+  left: v-bind(PROPS.left);
+}
+.objectInside {
+  width: v-bind(PROPS.width);
+  height: v-bind(PROPS.height);
+}
+</style>
