1fc1feef4a6b699fbf4799285f3e61eb6cc82c02
vitalii boiko <vukelan@gmail.com>
2025-01-10 01:56:35 +0200

    add button stick to viewport

--- src/home/details/ButtonPlayPause.vue
+++ src/home/details/ButtonPlayPause.vue
@@ -1,15 +1,38 @@
 <script setup>
+import { onMounted, onUnmounted, ref } from 'vue';
 import * as css from '../home.module.scss';
 import { textBlock } from './refs';
+import { WIDTH } from './etc';
+
+const refDivOuter = ref();
 
 const onClick = () => {
   textBlock.value++;
   console.log(`textBlock.value= ${textBlock.value}`);
 };
+const resize = 'resize';
+const onResize = () => {
+  const width = Math.ceil(
+    parseFloat(window.getComputedStyle(document.documentElement).fontSize) *
+      WIDTH
+  );
+  if (window.innerWidth < width) {
+    const { left } = refDivOuter.value?.getBoundingClientRect();
+    refDivOuter.value.style.left = `${Math.ceil(
+      (width - window.innerWidth) / 2
+    )}px`;
+  }
+};
+onMounted(() => {
+  window.addEventListener(resize, onResize);
+});
+onUnmounted(() => {
+  window.removeEventListener(resize, onResize);
+});
 </script>
 
 <template>
-  <div :class="[css.buttonHolderDiv]">
+  <div :ref="(el) => (refDivOuter = el)" :class="[css.buttonHolderDiv]">
     <button
       :class="[css.buttonPlayPause, $style.buttonPlayPause]"
       @click="onClick"
