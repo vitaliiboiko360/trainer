472e2960d98ee8e4768bfe3706c524a19655d93c
vitalii boiko <vukelan@gmail.com>
2025-01-10 19:55:04 +0200

    add svg utils

---
+++ src/home/details/svg.ts
@@ -0,0 +1,8 @@
+export function addPathLine(svgElment, x1, y1, x2, y2) {
+  let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path.setAttribute('d', `M${x1} ${y1}H${x2}`);
+  path.setAttribute('stroke', 'green');
+  path.setAttribute('stroke-width', '2px');
+  SVGElement.append(path);
+  return path;
+}
