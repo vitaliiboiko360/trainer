00721c883ec415361553d97c02c492c5ac10ffc4
vitalii boiko <vukelan@gmail.com>
2025-01-10 22:35:27 +0200

    react when font size is changing

--- src/home/details/TextInside.vue
+++ src/home/details/TextInside.vue
@@ -6,6 +6,7 @@ import * as css from '../home.module.scss';
 import {
   hihglightTextInside,
   incrementHiglightTextInside,
+  REM_IN_PX,
   textInside,
 } from './refs';
 import { TXT_LINES, shuffle } from './etc';
@@ -29,12 +30,43 @@ const startAnimation = (target) => {
 };
 
 shuffle(TXT_LINES);
-const txtLines = ref(TXT_LINES);
+
+const addPathToSvg = (parendDiv, firstLine, svgBackground) => {
+  let mapLinePosition = new Map();
+  const {
+    left: parentLeft,
+    right: parentRight,
+    top: parentTop,
+  } = parendDiv.getBoundingClientRect();
+  Array.from(firstLine.children).forEach((element) => {
+    let { left, bottom: childBottom, right } = element.getBoundingClientRect();
+    const bottom = Math.ceil(childBottom - parentTop);
+    let line = mapLinePosition.get(bottom);
+    const minLeft = Math.ceil(left - parentLeft);
+    const maxRight = Math.ceil(right - parentLeft);
+    if (line) {
+      mapLinePosition.set(bottom, {
+        minLeftX: Math.min(minLeft, line.minLeftX),
+        maxRightX: Math.max(maxRight, line.maxRightX),
+      });
+    } else {
+      mapLinePosition.set(bottom, {
+        minLeftX: minLeft,
+        maxRightX: maxRight,
+      });
+    }
+  });
+  console.log(`console= ${[...mapLinePosition.entries()]}`);
+
+  for (const [y, x] of mapLinePosition) {
+    addPathLine(svgBackground, x.minLeftX, y, x.maxRightX - x.minLeftX);
+  }
+};
 
 onMounted(() => {
   const { left, top, height, width } = refDiv.value.getBoundingClientRect();
   let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.setAttribute('width', width);
+  svg.setAttribute('width', `${Math.ceil(width)}`);
   svg.setAttribute('height', height);
   svg.setAttribute('viewBox', `0 0 ${Math.ceil(width)} ${Math.ceil(height)}`);
   // svg.style.left = left;
@@ -53,8 +85,9 @@ watch(textInside, () => {
   incrementHiglightTextInside();
 });
 
-watch(hihglightTextInside, () => {
+watch([hihglightTextInside, REM_IN_PX], () => {
   if (hihglightTextInside.value < 1) return;
+  refSvg.value.replaceChildren();
 
   const firstLine = refDiv.value.children.item(0);
   const words = firstLine.textContent.split(' ');
@@ -65,35 +98,7 @@ watch(hihglightTextInside, () => {
   });
   firstLine.replaceChildren(...spans);
 
-  let mapLinePosition = new Map();
-  const {
-    left: parentLeft,
-    right: parentRight,
-    top: parentTop,
-  } = refDiv.value.getBoundingClientRect();
-  Array.from(firstLine.children).forEach((element) => {
-    let { left, bottom: childBottom, right } = element.getBoundingClientRect();
-    const bottom = Math.ceil(parentTop - childBottom);
-    let line = mapLinePosition.get(bottom);
-    const minLeft = Math.ceil(parentLeft - left);
-    const maxRight = Math.ceil(parentRight - right);
-    if (line) {
-      mapLinePosition.set(bottom, {
-        minLeftX: Math.min(minLeft, line.minLeftX),
-        maxRightX: Math.max(maxRight, line.maxRightX),
-      });
-    } else {
-      mapLinePosition.set(bottom, {
-        minLeftX: minLeft,
-        maxRightX: maxRight,
-      });
-    }
-  });
-  console.log(`console= ${[...mapLinePosition.entries()]}`);
-
-  for (const [y, x] of mapLinePosition) {
-    addPathLine(refSvg.value, x.minLeftX, y, x.maxRightX - x.minLeftX);
-  }
+  addPathToSvg(refDiv.value, firstLine, refSvg.value);
 });
 </script>
 
