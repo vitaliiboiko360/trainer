9a15cf067f3d30de3a8fb828f38f1b603d36177c
vitalii boiko <vukelan@gmail.com>
2025-01-13 23:23:01 +0200

    divide blocks by number of characters

--- src/page/LinesBlockLayout.vue
+++ src/page/LinesBlockLayout.vue
@@ -7,30 +7,45 @@ import { currentPageBlock } from './state/currentPageBlock';
 import type { Ref } from 'vue';
 import cloneDeep from 'lodash.clonedeep';
 
+const CHARACTER_NUMBER_PER_BLOCK = 250;
 const to1BasedIndex = (index) => index + 1;
 
 const { lines } = defineProps(['lines']);
+
 type TextLineInfo = {
   text: string;
   start: number;
   end: number;
   endParagraph: boolean | undefined;
 };
-type LineAndIndex = { textLineInfo: TextLineInfo; index: number };
+type LineAndIndex = {
+  textLineInfo: TextLineInfo;
+  index: number;
+};
 let currentBlock: Array<LineAndIndex> = [];
 let pageBlocks: Array<Array<LineAndIndex>> = [];
 
+let characterCount = 0;
 lines.forEach((lineElement, index) => {
+  characterCount += lineElement.text.length;
   currentBlock.push({
     textLineInfo: lineElement,
     index: to1BasedIndex(index) /* LineSentence expects 1-based indexing */,
   });
-  if (lineElement.endParagraph) {
+  if (
+    characterCount >= CHARACTER_NUMBER_PER_BLOCK ||
+    lineElement.endParagraph
+  ) {
     pageBlocks.push(cloneDeep(currentBlock));
     currentBlock = [];
+    characterCount = 0;
   }
 });
-pageBlocks.push(cloneDeep(currentBlock));
+if (currentBlock.length > 0) {
+  pageBlocks.push(cloneDeep(currentBlock));
+}
+
+console.log(`number of blocks = ${pageBlocks.length}`);
 
 const totalNumberOfPages = pageBlocks.length - 1;
 
