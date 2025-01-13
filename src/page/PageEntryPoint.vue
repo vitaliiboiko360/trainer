be4a3343a06a95f3329947d310e01d5869d630e9
vitalii boiko <vukelan@gmail.com>
2025-01-13 17:31:49 +0200

    add page entry point to wrap quering list of text if it's not available

---
+++ src/page/PageEntryPoint.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useQuery } from '@tanstack/vue-query';
+
+import { LIST_TEXT_JSON_KEY, queryListOfTextJson } from '../query/default';
+import router from '../router/router';
+
+import Page from './Page.vue';
+import { computed } from 'vue';
+
+const idToIndex = (id) => id - 1;
+
+const { id, resource: resourceFromUrl } = defineProps(['id', 'resource']);
+
+const enabled = computed(() => resourceFromUrl === undefined);
+
+const { isPending, isError, data, error, isFetched } = useQuery({
+  queryKey: [LIST_TEXT_JSON_KEY],
+  queryFn: () => queryListOfTextJson(),
+  enabled,
+});
+router.replace({ query: null });
+</script>
+
+<template>
+  <Page v-if="resourceFromUrl" :id :resource="resourceFromUrl" />
+  <Page
+    v-else-if="isFetched"
+    :id
+    :resource="data.texts[idToIndex(id)].resource"
+  />
+  <span v-else-if="isPending">Loading...</span>
+  <span v-else-if="isError">Error {{ error }}</span>
+</template>
