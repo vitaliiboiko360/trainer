9dfad6194e00ce827cc50149caddb9cfbcd6447f
vitalii boiko <vukelan@gmail.com>
2025-01-25 16:03:23 +0200

    add new entry for admin editing lessons

---
+++ src/admin/AdminHome.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AdminListLessons from './AdminListLessons.vue';
+
+import { useQuery } from '@tanstack/vue-query';
+
+import { LIST_TEXT_JSON_KEY, queryListOfTextJson } from '../query/default';
+
+const { isPending, isFetched, isError, data, error } = useQuery({
+  queryKey: [LIST_TEXT_JSON_KEY],
+  queryFn: () => queryListOfTextJson(),
+});
+</script>
+
+<template>
+  <p v-if="isError">{{ error }}</p>
+  <p v-if="isPending">Getting lessons ...</p>
+  <AdminListLessons v-if="isFetched" :listOfLessons="data" />
+</template>
---
+++ src/admin/AdminListLessons.vue
@@ -0,0 +1,40 @@
+<script setup>
+// import {
+//   VCard,
+//   VToolbar,
+//   VBtn,
+//   VToolbarTitle,
+//   VSpacer,
+//   VList,
+// } from 'vuetify/components';
+import { defineProps } from 'vue';
+const { listOfLessons } = defineProps(['listOfLessons']);
+
+const items = [];
+
+listOfLessons.texts.forEach((textElement) => {
+  const item = { title: textElement.title };
+  items.push(item);
+  items.push({ type: 'divider', inset: true });
+});
+</script>
+
+<template>
+  <v-card class="mx-auto" max-width="450">
+    <v-toolbar color="cyan-lighten-1">
+      <v-btn icon="mdi-menu" variant="text"></v-btn>
+
+      <v-toolbar-title>Inbox</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon="mdi-magnify" variant="text"></v-btn>
+    </v-toolbar>
+
+    <v-list :items="items" lines="three" item-props>
+      <template v-slot:subtitle="{ subtitle }">
+        <div v-html="subtitle"></div>
+      </template>
+    </v-list>
+  </v-card>
+</template>
