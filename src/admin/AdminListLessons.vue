f1c189bb85e1e807b4ca439588c57730a1a6e55e
vitalii boiko <vukelan@gmail.com>
2025-01-25 19:26:00 +0200

    admin style list of lessons

--- src/admin/AdminListLessons.vue
+++ src/admin/AdminListLessons.vue
@@ -1,40 +1,55 @@
 <script setup>
-// import {
-//   VCard,
-//   VToolbar,
-//   VBtn,
-//   VToolbarTitle,
-//   VSpacer,
-//   VList,
-// } from 'vuetify/components';
+import {
+  VCard,
+  VToolbar,
+  VBtn,
+  VToolbarTitle,
+  VSpacer,
+  VList,
+} from 'vuetify/components';
 import { defineProps } from 'vue';
 const { listOfLessons } = defineProps(['listOfLessons']);
 
 const items = [];
 
 listOfLessons.texts.forEach((textElement) => {
-  const item = { title: textElement.title };
+  const item = { title: textElement.title, subtitle: '0%' };
   items.push(item);
-  items.push({ type: 'divider', inset: true });
+  items.push({ type: 'divider' });
 });
 </script>
 
+//min-width="450"
 <template>
-  <v-card class="mx-auto" max-width="450">
+  <v-card class="mx-auto" max-width="760">
     <v-toolbar color="cyan-lighten-1">
-      <v-btn icon="mdi-menu" variant="text"></v-btn>
-
-      <v-toolbar-title>Inbox</v-toolbar-title>
-
+      <v-toolbar-title
+        :class="$style.centerToolbar"
+        class="mx-lg-auto"
+        width="560px"
+        >List of Lessons</v-toolbar-title
+      >
       <v-spacer></v-spacer>
-
-      <v-btn icon="mdi-magnify" variant="text"></v-btn>
     </v-toolbar>
 
-    <v-list :items="items" lines="three" item-props>
+    <v-list
+      :items="items"
+      lines="three"
+      item-props
+      class="mx-auto"
+      max-width="560"
+    >
       <template v-slot:subtitle="{ subtitle }">
-        <div v-html="subtitle"></div>
+        <div>{{ subtitle }}</div>
       </template>
     </v-list>
   </v-card>
 </template>
+
+<style module>
+.centerToolbar {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
