a1d2f0cbb7314c023d06dd115f52649eeff86fff
vitalii boiko <vukelan@gmail.com>
2025-01-26 03:01:19 +0200

    add container for editor

--- src/admin/AdminEditLesson.vue
+++ src/admin/AdminEditLesson.vue
@@ -1,11 +1,17 @@
 <script setup></script>
 
 <template>
-  <v-sheet
-    :elevation="9"
-    :height="200"
-    :width="200"
-    border
-    border-radius="20px"
-  ></v-sheet>
+  <v-container class="bg-surface-variant">
+    <v-row justify="center" no-gutters>
+      <v-col align-self="center" cols="auto">
+        <v-sheet
+          :elevation="9"
+          :height="200"
+          :width="200"
+          border
+          border-radius="20px"
+        ></v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
