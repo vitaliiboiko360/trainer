41fd599a62efa6e61751ae4bda0f85b8ccb46b51
vitalii boiko <vukelan@gmail.com>
2025-01-28 14:00:06 +0200

    split form into lines

--- src/admin/AdminEditForm.vue
+++ src/admin/AdminEditForm.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { defineProps } from 'vue';
+import AdminEditFormLine from './AdminEditFormLine.vue';
 
 const { data } = defineProps(['data']);
 </script>
 
 <template>
-  <div>{{ data }}</div>
+  <AdminEditFormLine
+    v-for="line in data.lines"
+    :start="line.start"
+    :end="line.end"
+    :text="line.text"
+  />
 </template>
---
+++ src/admin/AdminEditFormLine.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineModel, defineProps } from 'vue';
+const { text: textLine } = defineProps(['text']);
+const start = defineModel('start');
+const end = defineModel('end');
+</script>
+
+<template>
+  <v-text-field
+    v-model="start"
+    density="compact"
+    style="width: 80px"
+    type="number"
+    variant="outlined"
+    hide-details
+  ></v-text-field>
+  <v-text-field
+    v-model="end"
+    density="compact"
+    style="width: 80px"
+    type="number"
+    variant="outlined"
+    hide-details
+  ></v-text-field>
+  <div>{{ textLine }}</div>
+</template>
