d5bb3890b15f0df84ea7a47c02955ee0fcd6a0bf
vitalii boiko <vukelan@gmail.com>
2025-01-29 14:34:48 +0200

    replace to css icons to svg

--- js/main.ts
+++ js/main.ts
@@ -6,6 +6,7 @@ import App from '../src/App.vue';
 
 import { VueQueryPlugin } from '@tanstack/vue-query';
 
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
@@ -23,6 +24,13 @@ const vuetify = createVuetify({
           'surface-variant': colors.indigo.lighten2, //rgb(96, 143, 160)
         },
       },
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
     },
   },
 });
