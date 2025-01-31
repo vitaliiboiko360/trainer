79d6de38e8eb9dd5e37fb8470f40dda13eeb9c67
vitalii boiko <vukelan@gmail.com>
2025-01-31 18:03:24 +0200

    add start end edit fields

--- src/admin/line_details/LineEditor.vue
+++ src/admin/line_details/LineEditor.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { defineProps } from 'vue';
+import AdminEditField from '../AdminEditField.vue';
 const { text: textLine, index } = defineProps([
   'text',
   'start',
@@ -10,15 +11,20 @@ const { text: textLine, index } = defineProps([
 const to1Based = (index) => index + 1;
 </script>
 
-text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat."
 <template>
   <v-expansion-panel :key="index">
     <template v-slot:title>
       <div>
         <p>{{ to1Based(index) + '.   ' + textLine }}</p>
+      </div>
+    </template>
+    <template v-slot:text>
+      <div style="width: 200px">
+        <p style="display: inline; margin: 0 10px">Start:</p>
+        <p style="display: inline; margin: 0 10px">End:</p>
+      </div>
+      <div style="width: 200px">
+        <AdminEditField :editStart="start" :editEnd="end" :fieldId="index" />
       </div>
     </template>
   </v-expansion-panel>
