941e7044ffb3545f3dd780b9b0a2664122eb00d2
vitalii boiko <vukelan@gmail.com>
2025-02-01 22:14:28 +0200

    load translations with lesson

--- src/admin/AdminQueryLesson.vue
+++ src/admin/AdminQueryLesson.vue
@@ -6,13 +6,23 @@ import AdminLessonWithTrans_New from './AdminLessonWithTrans_New.vue';
 
 const { id, resource } = defineProps(['id', 'resource']);
 const url = `/data/${resource}.json`;
+const urlTranslations = `/data/tr/${resource}.json`;
 
 const { isPending, isError, data, error, isFetched } = useQuery({
   queryKey: [`lesson-${id}`],
   queryFn: () => queryJson(url),
 });
+
+const { isFetched: isFetchedTranslations, data: translations } = useQuery({
+  queryKey: [`translations-${id}`],
+  queryFn: () => queryJson(urlTranslations),
+});
 </script>
 
 <template>
-  <AdminLessonWithTrans_New v-if="isFetched" :data />
+  <AdminLessonWithTrans_New
+    v-if="isFetched && isFetchedTranslations"
+    :data
+    :translations
+  />
 </template>
