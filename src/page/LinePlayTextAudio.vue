c8afa1f9f8badc14c0baa0486ccc4ede1c685bcf
vitalii boiko <vukelan@gmail.com>
2025-02-08 23:09:56 +0200

    add button for line

---
+++ src/page/LinePlayTextAudio.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <button :class="$style.buttonPlayAudio"></button>
+</template>
+
+<style module>
+.buttonPlayAudio {
+  height: 30px;
+  width: 40px;
+}
+</style>
