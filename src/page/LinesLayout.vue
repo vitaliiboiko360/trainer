f73f2a64119bb158e652d70e8b81d5e178549e8f
vitalii boiko <vukelan@gmail.com>
2025-02-10 16:07:58 +0200

    extract page slider text divider into separate component

--- src/page/LinesLayout.vue
+++ src/page/LinesLayout.vue
@@ -2,11 +2,22 @@
 import { textView, TEXTVIEW_BUTTONS } from './state/textView';
 import LinesBlockLayout from './layout/LinesBlockLayout.vue';
 import LinesPageLayout from './layout/LinesPageLayout.vue';
+import PageSliderDivider from './layout/PageSliderDivider.vue';
 
 const { lines } = defineProps(['lines']);
 </script>
 
 <template>
-  <LinesBlockLayout v-if="textView == TEXTVIEW_BUTTONS.LineMode" :lines />
-  <LinesPageLayout v-if="textView == TEXTVIEW_BUTTONS.ParagraphMode" :lines />
+  <PageSliderDivider :lines v-slot="slotProps">
+    <LinesBlockLayout
+      v-if="textView == TEXTVIEW_BUTTONS.LineMode"
+      :totalNumberOfPages="slotProps.totalNumberOfPages"
+      :displayedLines="slotProps.displayedLines"
+    />
+    <LinesPageLayout
+      v-if="textView == TEXTVIEW_BUTTONS.ParagraphMode"
+      :totalNumberOfPages="slotProps.totalNumberOfPages"
+      :displayedLines="slotProps.displayedLines"
+    />
+  </PageSliderDivider>
 </template>
---
+++ src/page/layout/PageSliderDivider.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import { watch, ref } from 'vue';
+import { currentPageBlock } from '../state/currentPageBlock';
+import type { Ref } from 'vue';
+import cloneDeep from 'lodash.clonedeep';
+
+const CHARACTER_NUMBER_PER_BLOCK = 250;
+const to1BasedIndex = (index) => index + 1;
+
+const { lines } = defineProps(['lines']);
+
+type TextLineInfo = {
+  text: string;
+  start: number;
+  end: number;
+  endParagraph: boolean | undefined;
+};
+type LineAndIndex = {
+  textLineInfo: TextLineInfo;
+  lineNumber: number;
+};
+let currentBlock: Array<LineAndIndex> = [];
+let pageBlocks: Array<Array<LineAndIndex>> = [];
+
+const putLinesInBlocks = (characterCountPerBlock) => {
+  let characterCount = 0;
+  currentBlock = [];
+  return (lineElement, index) => {
+    characterCount += lineElement.text.length;
+    currentBlock.push({
+      textLineInfo: lineElement,
+      lineNumber:
+        to1BasedIndex(index) /* LineSentence expects 1-based indexing */,
+    });
+    if (characterCount >= characterCountPerBlock) {
+      // if we exceeded num per block threshold, we're flushing lines to block
+      pageBlocks.push(cloneDeep(currentBlock));
+      currentBlock = [];
+      characterCount = 0;
+    }
+  };
+};
+
+const getAverageCharacterCount = (total, arrayOfLines, index) => {
+  const characterCountInBlock = arrayOfLines.reduce(
+    (total, line) => total + line.textLineInfo.text.length,
+    0
+  );
+  return Math.ceil(total + characterCountInBlock / to1BasedIndex(index));
+};
+
+lines.forEach(putLinesInBlocks(CHARACTER_NUMBER_PER_BLOCK));
+
+const getTotalCharacterCount = (total, line) => {
+  return total + line.textLineInfo.text.length;
+};
+
+let totalCharacterCountInLastBlock = currentBlock.reduce(
+  getTotalCharacterCount,
+  0
+);
+
+if (totalCharacterCountInLastBlock > 0) {
+  if (
+    totalCharacterCountInLastBlock <
+    0.5 * pageBlocks.reduce(getAverageCharacterCount, 0)
+  ) {
+    let stepToReduceCharacterLimitThreshold = 30;
+
+    do {
+      pageBlocks = [];
+
+      lines.forEach(
+        putLinesInBlocks(
+          CHARACTER_NUMBER_PER_BLOCK - stepToReduceCharacterLimitThreshold
+        )
+      );
+
+      totalCharacterCountInLastBlock = currentBlock.reduce(
+        getTotalCharacterCount,
+        0
+      );
+
+      stepToReduceCharacterLimitThreshold += 15;
+
+      if (stepToReduceCharacterLimitThreshold > 50) {
+        break;
+      }
+    } while (
+      totalCharacterCountInLastBlock <
+      0.5 * pageBlocks.reduce(getAverageCharacterCount, 0)
+    );
+  }
+
+  if (currentBlock.reduce(getTotalCharacterCount, 0) > 0) {
+    pageBlocks.push(cloneDeep(currentBlock));
+  }
+}
+
+const totalNumberOfPages = pageBlocks.length - 1;
+
+const displayedLines: Ref<Array<LineAndIndex>> = ref(
+  pageBlocks[currentPageBlock.value]
+);
+
+watch(currentPageBlock, (newUserSelectedCurrentPageBlock) => {
+  displayedLines.value = pageBlocks[newUserSelectedCurrentPageBlock];
+});
+</script>
+
+<template>
+  <slot :totalNumberOfPages :displayedLines></slot>
+</template>
