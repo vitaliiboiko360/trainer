e7153fc8cb987fc7d6da4373882198c163912885
vitalii boiko <vukelan@gmail.com>
2025-03-02 13:47:41 +0200

    add icon current play indicator

--- src/page/line/LinePlayIndicator.vue
+++ src/page/line/LinePlayIndicator.vue
@@ -3,12 +3,13 @@ const iconEmptyLine =
   'M6.5 17.5a.5.5 0 01.5-.5h2a.5.5 0 010 1h-2a.5.5 0 01-.5-.5m4 0a.5.5 0 01.5-.5h2a.5.5 0 010 1h-2a.5.5 0 01-.5-.5m4 0a.5.5 0 01.5-.5h2a.5.5 0 010 1h-2a.5.5 0 01-.5-.5';
 const icontCurrentWillBePlaying =
   'M11 14a.5.5 0 01.5-.5h3a.5.5 0 010 1h-3A.5.5 0 0111 14M8.854 9.146a.5.5 0 10-.708.708L9.793 11.5 8.146 13.146a.5.5 0 10.708.708l2-2a.5.5 0 000-.708z M6.5 17.5a.5.5 0 01.5-.5h2a.5.5 0 010 1h-2a.5.5 0 01-.5-.5m4 0a.5.5 0 01.5-.5h2a.5.5 0 010 1h-2a.5.5 0 01-.5-.5m4 0a.5.5 0 01.5-.5h2a.5.5 0 010 1h-2a.5.5 0 01-.5-.5';
+const currentPlayIcon =
+  'M6 9a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 6 9M3.854 4.146a.5.5 0 1 0-.708.708L4.793 6.5 3.146 8.146a.5.5 0 1 0 .708.708l2-2a.5.5 0 0 0 0-.708z';
 </script>
 
 <template>
   <div>
-    <v-icon :icon="iconEmptyLine"></v-icon>
-    <!-- <v-icon :icon="icontCurrentWillBePlaying"></v-icon> -->
+    <v-icon :icon="currentPlayIcon" size="24"></v-icon>
   </div>
 </template>
 
