c5d26d9104e1c164a00ad121b2227ceafef29f1c
vitalii boiko <vukelan@gmail.com>
2025-03-02 18:02:56 +0200

    change property name

--- src/store/indicatorIndex.ts
+++ src/store/indicatorIndex.ts
@@ -1,21 +1,21 @@
 import { defineStore } from 'pinia';
 export const useIndicatorIndexStore = defineStore('indicatorIndex', {
   state: () => ({
-    indicatorIndex: 0,
+    indicatorIndexState: 0,
   }),
   getters: {
-    indicatorIndex: (state) => state.indicatorIndex,
+    indicatorIndex: (state) => state.indicatorIndexState,
   },
   actions: {
     // since we rely on `this`, we cannot use an arrow function
     update(value) {
-      this.indicatorIndex = value;
+      this.indicatorIndexState = value;
     },
     updateToNext() {
-      this.indicatorIndex++;
+      this.indicatorIndexState++;
     },
     updateToPrev() {
-      this.indicatorIndex--;
+      this.indicatorIndexState--;
     },
   },
 });
