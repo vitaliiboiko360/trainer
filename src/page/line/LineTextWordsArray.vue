42af443e5b375cf34e5e3283b456221ae2df7739
vitalii boiko <vukelan@gmail.com>
2025-03-07 12:03:51 +0200

    add word array

---
+++ src/page/line/LineTextWordsArray.vue
@@ -0,0 +1,9 @@
+<script setup>
+const { textLine } = defineProps(['textLine']);
+</script>
+
+<template>
+  <span v-for="(word, index) of textLine.split(' ')" :key="index"
+    >{{ `${word} ` }}
+  </span>
+</template>
