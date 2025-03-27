e83d5424569e7b7aed7cb1bab8604e563e58f805
vitalii boiko <vukelan@gmail.com>
2025-03-27 17:57:46 +0200

    add text animation

--- src/home/banner/Slide.vue
+++ src/home/banner/Slide.vue
@@ -2,20 +2,48 @@
 import { onMounted, ref } from 'vue';
 import gsap from 'gsap';
 
+const words = ['Learn', 'Spanish', 'Online'];
+
 const refImg = ref();
+const refText = ref([]);
+
+const animateText = (target, leftShift) => {
+  gsap.to(target, {
+    opacity: 1,
+    duration: 0.7,
+    marginLeft: leftShift,
+  });
+};
+
 onMounted(() => {
   gsap.to(refImg.value, {
     opacity: 1,
     duration: 0.8,
     // ease: ,
     left: '+=120',
+    onComplete: () => {
+      refImg.value.style.opacity = 1;
+      Array.from({ length: 3 }).map((_, index) => {
+        setTimeout(() => {
+          animateText(refText.value[index], `+=${25 + 45 * (index + 1)}`);
+        }, 100 * (index + 1));
+      });
+    },
   });
 });
 </script>
 
 <template>
   <div :class="$style.container">
-    <p :class="$style.text">Learn Spanish Online</p>
+    <div :class="$style.text">
+      <p
+        v-for="word of words"
+        :ref="(el) => refText.push(el)"
+        style="opacity: 0"
+      >
+        {{ word }}
+      </p>
+    </div>
     <img
       :ref="(el) => (refImg = el)"
       :class="$style.image"
@@ -31,6 +59,9 @@ onMounted(() => {
 }
 .text {
   position: absolute;
+  left: 36px;
+  top: 53px;
+  font-family: 'Nunito';
 }
 .image {
   position: absolute;
