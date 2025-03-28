aad837441a0478551d8f69f01d9c584336c8c6eb
vitalii boiko <vukelan@gmail.com>
2025-03-28 19:20:29 +0200

    add text bubble comp

---
+++ src/home/banner/TextBubble.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted, getCurrentInstance } from 'vue';
+
+const textMessages = [
+  'Este estudiante tiene un libro',
+  'Él también tiene una pluma',
+  'Es domingo por la mañana',
+];
+
+const createWordElement = (textToInsert, targetToInsertTo) => {
+  const pElement = document.createElement('span');
+  pElement.style.opacity = '0';
+  pElement.textContent = textToInsert;
+  targetToInsertTo.value.append(pElement);
+  return pElement;
+};
+
+onMounted(() => {
+  const instance = getCurrentInstance();
+  console.log(instance.vnode.el.parentNode);
+});
+</script>
+
+<template></template>
+
+<style module></style>
