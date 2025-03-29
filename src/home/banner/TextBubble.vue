eb3d526e99326cd9da9d5b3ad6d352b862fa6557
vitalii boiko <vukelan@gmail.com>
2025-03-29 14:03:35 +0200

    correct text bubble animation

--- src/home/banner/TextBubble.vue
+++ src/home/banner/TextBubble.vue
@@ -9,9 +9,10 @@ const refPText = ref();
 
 const colorValues = [
   '#E6F4EA',
-  '#F48125',
+  '#ffeade',
   '#D4E8FA',
   '#C4C0FF',
+  '#f8ab6e',
   '#BDBEFC',
   '#FCBA03',
 ];
@@ -66,16 +67,34 @@ watch(textMsgOrder, () => {
       arrayOfCharacterSpans
     );
 
-    const animateChar = (index) => {
-      const characterSpan = arrayOfCharacterSpans[index];
+    const animateChar = (charIndex) => {
+      const characterSpan = arrayOfCharacterSpans[charIndex];
       gsap.to(characterSpan, {
         opacity: 1,
-        duration: 0.1,
+        duration: 0.13,
         ease: 'none',
         onComplete: () => {
-          const newIndex = index + 1;
-          if (newIndex < arrayOfCharacterSpans.length) {
-            animateChar(newIndex);
+          const newCharIndex = charIndex + 1;
+          if (newCharIndex < arrayOfCharacterSpans.length) {
+            animateChar(newCharIndex);
+          } else {
+            spanElement.style.boxShadow = 'unset';
+            spanElement.style.backgroundColor = 'unset';
+            const newIndex = index + 1;
+            if (newIndex < words.length) {
+              setTimeout(() => animateWord(words[newIndex], newIndex), 70);
+            } else {
+              gsap.to(refPText.value, { opacity: 0, duration: 0.7 });
+              setTimeout(() => {
+                while (refPText.value.firstChild) {
+                  refPText.value.removeChild(refPText.value.lastChild);
+                }
+              }, 700);
+              setTimeout(() => {
+                textMsgOrder.value =
+                  (textMsgOrder.value + 1) % textMessages.length;
+              }, 800);
+            }
           }
         },
       });
@@ -88,30 +107,23 @@ watch(textMsgOrder, () => {
     const updatedObject = { key: 0 };
 
     gsap.to(updatedObject, {
-      duration: 0.6,
-      key: 3,
+      duration: 0.5,
+      key: 5,
       yoyo: true,
+      repeat: 1,
+      yoyoEase: 'none', // 'elastic.out',
+      ease: 'none',
       onUpdate: () => {
-        spanElement.style.boxShadow = `0 0 0 ${updatedObject.key} ${color}`;
+        spanElement.style.boxShadow = `0 0 0 ${updatedObject.key}px ${color}`;
       },
       onComplete: () => {
-        spanElement.style.backgroundColor = 'unset';
-        spanElement.style.boxShadow = 'unset';
+        updatedObject.key = 0;
+        //   const newIndex = index + 1;
+        //   if (newIndex < words.length) {
+        //     // animateWord(words[newIndex], newIndex);
+        //   } else {
 
-        const newIndex = index + 1;
-        if (newIndex < words.length) {
-          animateWord(words[newIndex], newIndex);
-        } else {
-          gsap.to(refPText.value, { opacity: 0, duration: 0.7 });
-          setTimeout(() => {
-            while (refPText.value.firstChild) {
-              refPText.value.removeChild(refPText.value.lastChild);
-            }
-          }, 700);
-          setTimeout(() => {
-            textMsgOrder.value = (textMsgOrder.value + 1) % textMessages.length;
-          }, 800);
-        }
+        //   }
       },
     });
   };
@@ -127,5 +139,9 @@ watch(textMsgOrder, () => {
 <style module>
 .pText {
   white-space: pre-wrap;
+  span {
+    border-radius: 6px;
+    text-shadow: #ffffff 0 0 10px;
+  }
 }
 </style>
