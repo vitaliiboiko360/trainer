0561780ebe679d2d9fd26a23bf4940e29722feae
vitalii boiko <vukelan@gmail.com>
2025-03-30 02:01:08 +0200

    add is audio ready icon

---
+++ src/admin/line_details/ReadyIcon.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {
+  mdiCheckboxMarkedCircleOutline,
+  mdiCheckboxBlankCircleOutline,
+} from '@mdi/js';
+const { isAudioReady } = defineProps(['isAudioReady']);
+</script>
+
+<template>
+  <v-icon
+    v-if="isAudioReady"
+    size="24"
+    color="success"
+    :icon="mdiCheckboxMarkedCircleOutline"
+  ></v-icon>
+  <v-icon
+    v-else
+    size="24"
+    color="info"
+    :icon="mdiCheckboxBlankCircleOutline"
+  ></v-icon>
+  <span>&nbsp;&nbsp;</span>
+</template>
+
+<style module></style>
