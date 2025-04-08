95f530d1b53f4c664e2dc360a5c7e8fdb592ca29
vitalii boiko <vukelan@gmail.com>
2025-04-09 02:01:38 +0300

    replace line editor props to reactive object

--- src/admin/AdminLessonWithTranslations.vue
+++ src/admin/AdminLessonWithTranslations.vue
@@ -1,18 +1,30 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, reactive } from 'vue';
 import LineEditor from './line_details/LineEditor.vue';
 
 const { data, translations: tr } = defineProps(['data', 'translations']);
+
+const lines = data.lines.map((line, index) => {
+  return reactive({
+    start: line.start,
+    end: line.end,
+    text: line.text,
+    lineNumber: line.lineNumber ?? index,
+    speechParts: line.speechParts ?? [],
+    translations: line.translations ?? [],
+  });
+});
 </script>
 
 <template>
   <div class="my-6">
     <v-expansion-panels>
       <LineEditor
-        v-for="(line, index) in data.lines"
+        v-for="(line, index) in lines"
         :start="line.start"
         :end="line.end"
         :text="line.text"
+        :lineNumber="line.lineNumber"
         :index
         :translationAndOriginalLIne="tr?.translations?.at(index)"
       />
