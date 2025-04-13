b49160bf014d765cb2eb564cf8fb69ab4335bb7a
vitalii boiko <vukelan@gmail.com>
2025-04-14 02:37:49 +0300

    component for line editor title

---
+++ src/admin/line_details/LineEditorTitle.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps(['line', 'selectedClass']);
+</script>
+
+<template>
+  <span v-for="word in line.split(' ')" :class="selectedClass"
+    >{{ word }}<span> </span
+  ></span>
+</template>
+
+<style module></style>
