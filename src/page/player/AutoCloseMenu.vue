fab25e895bc4380b164effb446d3aa2bd37012e7
vitalii boiko <vukelan@gmail.com>
2025-04-23 02:47:39 +0300

    add auto close menu component

---
+++ src/page/player/AutoCloseMenu.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+const emit = defineEmits(['close']);
+
+const refMenuContainer = ref();
+
+const onClickCheckIfOutside = (event) => {
+  if (refMenuContainer.value.contains(event.target) != true) {
+    emit('close');
+  }
+};
+
+onMounted(() => {
+  document.addEventListener('click', onClickCheckIfOutside);
+});
+
+onBeforeUnmount(() => {
+  document.addEventListener('click', onClickCheckIfOutside);
+});
+</script>
+
+<template>
+  <div :ref="(el) => (refMenuContainer = el)">
+    <slot></slot>
+  </div>
+</template>
+
+<style module></style>
