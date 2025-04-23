16f5fc36725d7f63bc30523f636262790c82daff
vitalii boiko <vukelan@gmail.com>
2025-04-23 20:30:32 +0300

    init inidcator at first line

--- src/page/LinesLayout.vue
+++ src/page/LinesLayout.vue
@@ -1,10 +1,16 @@
 <script setup>
+import { onBeforeMount } from 'vue';
 import { textView, TEXTVIEW_BUTTONS } from './state/textView';
 import LinesBlockLayout from './layout/LinesBlockLayout.vue';
 import LinesPageLayout from './layout/LinesPageLayout.vue';
 import PageSliderDivider from './layout/PageSliderDivider.vue';
+import { useIndicatorIndexStore } from '../store/indicatorIndex';
 
+const indicatorIndexStore = useIndicatorIndexStore();
 const { lines } = defineProps(['lines']);
+onBeforeMount(() => {
+  indicatorIndexStore.update(1);
+});
 </script>
 
 <template>
