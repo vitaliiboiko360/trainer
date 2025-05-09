29715594867c1e08f9f921e537e85ba96209a168
vitalii boiko <vukelan@gmail.com>
2025-05-09 23:29:14 +0300

    add detect play pause event

--- src/page/player/PlayPauseButton.vue
+++ src/page/player/PlayPauseButton.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { detectPlayButtonEvent } from '../state/playTime';
 const iconPlay2 =
   'M19.5712 11.8043 7.1498 4.5984v14.4118zm1.695-1.4896a1.7164 1.7164 90 010 2.9791l-13.6178 7.9015C6.5355 21.8417 5.0096 21.1033 5.0096 19.7058V3.9028c0-1.3975 1.5259-2.1359 2.6388-1.4896z';
 const iconPlay1 =
@@ -18,6 +18,9 @@ const iconPause =
 import { isPlay } from '../state/playTime';
 const onClick = () => {
   isPlay.value = !isPlay.value;
+  if (isPlay.value) {
+    detectPlayButtonEvent.value = !detectPlayButtonEvent.value;
+  }
 };
 </script>
 
