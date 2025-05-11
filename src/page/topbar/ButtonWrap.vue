8e32e12f7be4493d4059476cc5d2fc40120eb8d5
vitalii boiko <vukelan@gmail.com>
2025-05-11 22:36:58 +0300

    move out to seprate wrapper inside

--- src/page/topbar/ButtonWrap.vue
+++ src/page/topbar/ButtonWrap.vue
@@ -1,12 +1,14 @@
-<script setup></script>
+<script setup>
+import ButtonWrapSlot from './ButtonWrapSlot.vue';
+</script>
 
 <template>
   <div :class="$style.divOuter">
-    <slot name="button">
-      <span :class="$style.spanOuter"
-        ><span :class="$style.spanInner"></span
-      ></span>
-    </slot>
+    <ButtonWrapSlot>
+      <template v-slot:button>
+        <slot name="button"></slot>
+      </template>
+    </ButtonWrapSlot>
   </div>
 </template>
 
@@ -21,45 +23,5 @@
 }
 .divOuter {
   animation: partialFadeIn 0.3s;
-}
-.spanOuter {
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-  display: inline-block;
-  padding: 0 6px;
-}
-@keyframes spanInnerAnimation {
-  50% {
-    background-position: 140% 50%;
-    transform: skew(-2deg);
-  }
-}
-.spanInner {
-  position: absolute;
-  top: 1px;
-  right: 1px;
-  bottom: 1px;
-  left: 1px;
-  background: inherit;
-  border-radius: 6px;
-  background: linear-gradient(-90deg, #007cf0, #00dfd8, #ff0080, #007cf0);
-  background-size: 400% 100%;
-  border: none;
-  padding: 0;
-  margin: 0;
-  animation: spanInnerAnimation 8s ease-in-out infinite;
-  ::after {
-    content: '';
-    position: absolute;
-    background-size: inherit;
-    background-image: inherit;
-    animation: inherit;
-    left: 0;
-    right: 0;
-    top: 2px;
-    height: 100%;
-    filter: blur(0.5rem);
-  }
 }
 </style>
