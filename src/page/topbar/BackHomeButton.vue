8f102175a25fb0baeed4655ffc098aee33a8826d
vitalii boiko <vukelan@gmail.com>
2025-05-11 22:47:45 +0300

    pass insides to button

--- src/page/topbar/BackHomeButton.vue
+++ src/page/topbar/BackHomeButton.vue
@@ -3,6 +3,7 @@ import css from '../page.module.scss';
 import cssTopbar from './topbar.module.scss';
 import { useRouter } from 'vue-router';
 import ButtonWrap from './ButtonWrap.vue';
+import ButtonWrapInside from './ButtonWrapInside.vue';
 const router = useRouter();
 const onClick = () => {
   router.push({ path: '/' });
@@ -15,7 +16,9 @@ const onClick = () => {
       <button
         :class="[css.topBarButtonBackHome, cssTopbar.topbarButtonBase]"
         @click="onClick"
-      ></button>
+      >
+        <ButtonWrapInside />
+      </button>
     </template>
   </ButtonWrap>
 </template>
