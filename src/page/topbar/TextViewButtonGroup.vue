c85546cbb40f0da09978cdd2540d42a7f9d9cbe0
vitalii boiko <vukelan@gmail.com>
2025-05-12 22:36:13 +0300

    move style inside component

--- src/page/topbar/TextViewButtonGroup.vue
+++ src/page/topbar/TextViewButtonGroup.vue
@@ -3,42 +3,45 @@ import { nextTick, ref } from 'vue';
 import css from '../page.module.scss';
 import { textView, TEXTVIEW_BUTTONS as buttons } from '../state/textView.ts';
 
+const textMode = ref(buttons.LineMode);
+
 const onClick = (value) => {
   textView.value = value;
-  nextTick();
-  switch (value) {
-    case buttons.LineMode:
-      paragraphModeClassList.value = paragraphModeClassList.value.slice(0, 2);
-      return lineModeClassList.value.push(css.selectedButton);
-    case buttons.ParagraphMode:
-      lineModeClassList.value = lineModeClassList.value.slice(0, 2);
-      return paragraphModeClassList.value.push(css.selectedButton);
+  if (value == buttons.LineMode) {
+    textMode.value = buttons.LineMode;
+  } else if (value == buttons.ParagraphMode) {
+    textMode.value = buttons.ParagraphMode;
   }
 };
-
-let lineModeClassList = ref([
-  css.topBarGroupedButtonLeft,
-  css.topBarButtonLineOnTheirOwn,
-  css.selectedButton,
-]);
-let paragraphModeClassList = ref([
-  css.topBarGroupedButtonRight,
-  css.topBarButton_TextOrdinary,
-]);
 </script>
 
 <template>
   <button
-    :class="[lineModeClassList, $style.topbarButtonBaseLeft]"
+    :class="[
+      { [$style.selectedButton]: textMode == buttons.LineMode },
+      $style.topbarButtonBaseLeft,
+      css.topBarGroupedButtonLeft,
+      css.topBarButtonLineOnTheirOwn,
+    ]"
     @click="() => onClick(buttons.LineMode)"
   ></button>
   <button
-    :class="[paragraphModeClassList, $style.topbarButtonBaseRight]"
+    :class="[
+      { [$style.selectedButton]: textMode == buttons.ParagraphMode },
+      $style.topbarButtonBaseRight,
+      css.topBarGroupedButtonRight,
+      css.topBarButton_TextOrdinary,
+    ]"
     @click="() => onClick(buttons.ParagraphMode)"
   ></button>
 </template>
 
 <style module>
+.selectedButton {
+  &:before {
+    background-color: #eee;
+  }
+}
 .topbarButtonBaseLeft {
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
