36879ce2de44f0b64aba63d996816ab11e6737ee
vitalii boiko <vukelan@gmail.com>
2025-05-13 21:44:46 +0300

    add visuals to text mode button group

--- src/page/topbar/TextViewButtonGroup.vue
+++ src/page/topbar/TextViewButtonGroup.vue
@@ -16,24 +16,40 @@ const onClick = (value) => {
 </script>
 
 <template>
-  <button
-    :class="[
-      { [$style.selectedButton]: textMode == buttons.LineMode },
-      $style.topbarButtonBaseLeft,
-      css.topBarGroupedButtonLeft,
-      css.topBarButtonLineOnTheirOwn,
-    ]"
-    @click="() => onClick(buttons.LineMode)"
-  ></button>
-  <button
-    :class="[
-      { [$style.selectedButton]: textMode == buttons.ParagraphMode },
-      $style.topbarButtonBaseRight,
-      css.topBarGroupedButtonRight,
-      css.topBarButton_TextOrdinary,
-    ]"
-    @click="() => onClick(buttons.ParagraphMode)"
-  ></button>
+  <div :class="$style.divOuter">
+    <button
+      :class="[
+        { [$style.selectedButton]: textMode == buttons.LineMode },
+        $style.topbarButtonBaseLeft,
+        css.topBarGroupedButtonLeft,
+        css.topBarButtonLineOnTheirOwn,
+      ]"
+      @click="() => onClick(buttons.LineMode)"
+    >
+      <span :class="$style.spanOuter"
+        ><span :class="$style.spanInner"></span
+      ></span>
+      <span :class="[$style.borderRadiusLeft, $style.spanCover]"></span>
+      <span :class="[$style.borderRadiusLeft, $style.spanCover2]"></span>
+      <span :class="[$style.borderRadiusLeft, $style.icon]"></span>
+    </button>
+    <button
+      :class="[
+        { [$style.selectedButton]: textMode == buttons.ParagraphMode },
+        $style.topbarButtonBaseRight,
+        css.topBarGroupedButtonRight,
+        css.topBarButton_TextOrdinary,
+      ]"
+      @click="() => onClick(buttons.ParagraphMode)"
+    >
+      <span :class="$style.spanOuter"
+        ><span :class="$style.spanInner"></span
+      ></span>
+      <span :class="[$style.borderRadiusLeft, $style.spanCover]"></span>
+      <span :class="[$style.borderRadiusLeft, $style.spanCover2]"></span>
+      <span :class="[$style.borderRadiusLeft, $style.icon]"></span>
+    </button>
+  </div>
 </template>
 
 <style module>
@@ -49,5 +65,120 @@ const onClick = (value) => {
 .topbarButtonBaseRight {
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+}
+
+.borderRadiusLeft {
+  border-radius: 16px 0 0 16px;
+}
+.borderRadiusRadius {
+  border-radius: 0 16px 16px 0;
+}
+
+.leftLineMode {
+  background-image: url(svg/list-check.svg);
+}
+
+.rightParagraphMode {
+  background-image: url(svg/text_ordinary.svg);
+}
+
+.icon {
+  background-size: contain;
+  background-repeat: space;
+  position: absolute;
+  top: -1px;
+  right: -1px;
+  bottom: -1px;
+  left: -1px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.spanCover2 {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.spanCover {
+  position: absolute;
+  top: -1px;
+  right: -1px;
+  bottom: -1px;
+  left: -1px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  filter: blur(1px);
+}
+.spanOuter {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  display: inline-block;
+  /* padding: 0 6px; */
+  background-color: #fff;
+}
+@keyframes spanInnerAnimation {
+  50% {
+    background-position: 100% 50%;
+    transform: skew(-2deg);
+  }
+}
+.oldSpanInner {
+  background: linear-gradient(-90deg, #007cf0, #00dfd8, #ff0080, #007cf0);
+}
+.spanInner {
+  position: absolute;
+  top: 1px;
+  right: 1px;
+  bottom: 1px;
+  left: 1px;
+  background: inherit;
+  border-radius: 16px;
+  background: linear-gradient(
+    -90deg,
+    #007cf0,
+    #fcf435,
+    #00df64,
+    #fa2a00,
+    #007cf0
+  );
+  background-size: 400% 100%;
+  border: none;
+  padding: 0;
+  margin: 0;
+  animation: spanInnerAnimation 48s ease-in-out infinite;
+  &:after {
+    content: '';
+    position: absolute;
+    background-size: inherit;
+    background-image: inherit;
+    animation: inherit;
+    left: 0;
+    right: 0;
+    top: 2px;
+    height: 100%;
+    filter: blur(0.32rem);
+    border-radius: 16px;
+  }
+}
+
+@keyframes partialFadeIn {
+  0% {
+    opacity: 0.6;
+  }
+  to {
+    opacity: 1;
+  }
+}
+.divOuter {
+  animation: partialFadeIn 0.3s;
 }
 </style>
