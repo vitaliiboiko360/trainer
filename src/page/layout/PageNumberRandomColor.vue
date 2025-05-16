b807919d1647ad4ad5425bf4699815c2fe58a52b
vitalii boiko <vukelan@gmail.com>
2025-05-16 23:39:59 +0300

    add reference colors

--- src/page/layout/PageNumberRandomColor.vue
+++ src/page/layout/PageNumberRandomColor.vue
@@ -1,5 +1,23 @@
-<script setup></script>
+<script setup>
+import PageNumber from './PageNumber.vue';
+const { index, isActive } = defineProps(['index', 'isActive']);
 
-<template></template>
+const COLORS = ['#f000a0', '#35acfc', '#00d0df', '#4b00fa', '#9cf000'];
+</script>
 
-<style module></style>
+<template>
+  <PageNumber :index :isActive />
+</template>
+
+<style module>
+.referenceColors {
+  background: linear-gradient(
+    -90deg,
+    #35acfc,
+    #4b00fa,
+    #f000a0,
+    #00d0df,
+    #9cf000
+  );
+}
+</style>
