83b0923514441feec277c30fdcf535332927a7ec
vitalii boiko <vukelan@gmail.com>
2025-05-17 01:35:50 +0300

    add shuffle array function

--- src/page/layout/PageNumberRandomColor.vue
+++ src/page/layout/PageNumberRandomColor.vue
@@ -3,6 +3,23 @@ import PageNumber from './PageNumber.vue';
 const { index, isActive } = defineProps(['index', 'isActive']);
 
 const COLORS = ['#f000a0', '#35acfc', '#00d0df', '#4b00fa', '#9cf000'];
+
+const getRandomNumber = (maxValue) => {
+  return Math.floor(Math.random() * maxValue);
+};
+const getShufledArray = () => {
+  const colors = [...COLORS];
+  for (let i = colors.length; i > 0; i--) {
+    let index = getRandomNumber(i);
+    while (index == i) {
+      index = getRandomNumber(i);
+    }
+    let temp = colors[index];
+    colors[index] = colors[i];
+    colors[i] = temp;
+  }
+  return colors;
+};
 </script>
 
 <template>
