c1cc13405e10734d8f64ea650bf6999a4c5c897b
vitalii boiko <vukelan@gmail.com>
2025-05-19 22:09:51 +0300

    complete styles

--- src/home/banner/WordSpan.vue
+++ src/home/banner/WordSpan.vue
@@ -3,16 +3,22 @@ import { ref } from 'vue';
 
 const { textToInsert } = defineProps(['textToInsert']);
 const colors = ['#E0F2FD', '#D7F1F1', '#c4c0ff'];
-const choosedColor = ref(colors[~~(Math.random() * colors.length)]);
+const choosenColor = ref(colors[~~(Math.random() * colors.length)]);
 </script>
 
 <template>
-  <span :class="$style.wordSpan">{{ textToInsert }}</span>
+  <p :class="$style.initOpacity">
+    <span :class="$style.wordSpan">{{ textToInsert }}</span>
+  </p>
 </template>
 
 <style module>
+.initOpacity {
+  opacity: 0;
+}
 .wordSpan {
-  background: linear-gradient(v-bind(choosedColor) 85%, transparent 0);
+  background: linear-gradient(v-bind(choosenColor) 85%, transparent 0);
   border-radius: 2px;
+  padding: 0 5px;
 }
 </style>
