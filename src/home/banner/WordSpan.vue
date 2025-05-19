06a3411f65231b4c1498ee185a8f96b0fe3d6c8b
vitalii boiko <vukelan@gmail.com>
2025-05-19 22:07:15 +0300

    add component for word container

---
+++ src/home/banner/WordSpan.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+
+const { textToInsert } = defineProps(['textToInsert']);
+const colors = ['#E0F2FD', '#D7F1F1', '#c4c0ff'];
+const choosedColor = ref(colors[~~(Math.random() * colors.length)]);
+</script>
+
+<template>
+  <span :class="$style.wordSpan">{{ textToInsert }}</span>
+</template>
+
+<style module>
+.wordSpan {
+  background: linear-gradient(v-bind(choosedColor) 85%, transparent 0);
+  border-radius: 2px;
+}
+</style>
