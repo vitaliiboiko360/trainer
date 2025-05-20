35e131301397d78fb5cc2050861d83aeb51a2f45
vitalii boiko <vukelan@gmail.com>
2025-05-20 23:05:43 +0300

    add component file

---
+++ src/home/banner/SlideCircles.vue
@@ -0,0 +1,35 @@
+<script setup>
+const { numberOfSlides = 5 } = defineProps(['numberOfSlides']);
+</script>
+
+<template>
+  <div :class="$style.container">
+    <div :class="$style.flexDiv">
+      <div v-for="i in numberOfSlides" :class="$style.item" :key="i">
+        <p>.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style module>
+.container {
+  position: absolute;
+  width: fit-content;
+  height: fit-content;
+  left: 0;
+  top: 0;
+}
+.flexDiv {
+  display: flex;
+  position: absolute;
+  bottom: 0;
+  flex-direction: row;
+  align-items: center;
+}
+.item {
+  height: 10px;
+  width: 10px;
+  background-color: #808080c9;
+}
+</style>
