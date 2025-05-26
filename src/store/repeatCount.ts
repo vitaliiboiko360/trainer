7348ecd326c3d430eb265457fed83a92bbcde094
vitalii boiko <vukelan@gmail.com>
2025-05-26 22:42:29 +0300

    add repeat count store

---
+++ src/store/repeatCount.ts
@@ -0,0 +1,42 @@
+import { defineStore } from 'pinia';
+
+const enum REPEAT_COUNT {
+  INITIAL = 0,
+  ONE,
+  TWO,
+  THREE,
+};
+
+export type RC = REPEAT_COUNT;
+
+export const useRepeatCountStore = defineStore('repeatCount', {
+  state: () => ({
+    repeatCount: REPEAT_COUNT.ONE,
+    repeatCountCurrent: REPEAT_COUNT.INITIAL,
+  }),
+  getters: {
+    repeatCount: (state) => {
+      return {
+        repeatCount: state.repeatCount,
+      };
+    },
+    repeatCountCurrent: (state) => {
+      return {
+        repeatCountCurrent: state.repeatCountCurrent;
+      }
+    },
+  },
+  actions: {
+    setRepeatCount(newRepeatCount: REPEAT_COUNT) {
+      this.repeatCount = newRepeatCount;
+    },
+    incrementCountCurrent(): Boolean {
+      if (this.repeatCountCurrent + 1 == this.repeatCount) {
+        this.repeatCountCurrent = REPEAT_COUNT.INITIAL;
+        return true;
+      }
+      this.repeatCountCurrent = this.repeatCountCurrent + 1;
+      return false;
+    }
+  },
+});
