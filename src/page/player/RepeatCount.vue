7458a80588eec9855721492f96b05c78251e6797
vitalii boiko <vukelan@gmail.com>
2025-05-27 21:59:09 +0300

    set repeat count on menu change

--- src/page/player/RepeatCount.vue
+++ src/page/player/RepeatCount.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { ref } from 'vue';
-
+import { ref, watch } from 'vue';
 import AutoCloseMenu from './AutoCloseMenu.vue';
+
+import { useRepeatCountStore } from '../../store/repeatCount';
+const repeatCountStore = useRepeatCountStore();
 
 const iconOne =
   'M 15.6 8 v 1.759 a 0.3 0.3 90 0 0 0.492 0.23 l 2.832 -2.359 a 0.3 0.3 90 0 0 0 -0.461 l -2.832 -2.359 a 0.3 0.3 90 0 0 -0.492 0.23 V 6.8 H 8.4 a 6 6 90 0 0 -5.376 8.668 a 0.6 0.6 90 0 0 1.075 -0.535 A 4.8 4.8 90 0 1 8.4 8 z m 5.376 2.132 a 0.6 0.6 90 0 0 -1.075 0.535 A 4.8 4.8 90 0 1 15.6 17.6 H 8.401 v -1.759 a 0.3 0.3 90 0 0 -0.492 -0.23 l -2.832 2.359 a 0.3 0.3 90 0 0 0 0.461 l 2.832 2.359 a 0.3 0.3 90 0 0 0.492 -0.23 V 18.8 h 7.2 a 6 6 90 0 0 5.376 -8.668 Z M 13.2 9.8 a 0.6 0.6 90 0 0 -1.025 -0.425 l -2.1 2.1 a 0.6 0.6 90 1 0 0.85 0.85 L 12 11.248 V 15.8 a 0.6 0.6 90 0 0 1.2 0 z';
@@ -28,6 +30,10 @@ const onClick2 = () => {
   repeatCountMenuItem2.value = repeatCount.value;
   repeatCount.value = menuItemRepeatCount;
 };
+
+watch(repeatCount, (newRepeatCount) => {
+  repeatCountStore.setRepeatCount(newRepeatCount);
+});
 </script>
 
 <template>
