8bf453f6947e41a93746c0e8273bcded5f5e374e
vitalii boiko <vukelan@gmail.com>
2025-06-01 00:25:21 +0300

    add wrapper and styles

--- src/page/Page.vue
+++ src/page/Page.vue
@@ -20,12 +20,22 @@ const { isPending, isError, data, error, isFetched } = useQuery({
   <span v-if="isPending">Loading Page...</span>
   <span v-else-if="isError">Error: {{ error.message }}</span>
   <div v-else :class="[css.divPage, $style.page]">
-    <TextPage :data />
+    <div :class="[$style.wrapper]">
+      <TextPage :data />
+    </div>
   </div>
 </template>
 
 <style module>
 .page {
-  background-color: aliceblue;
+  background-color: rgba(240, 248, 255, 0.8);
+  justify-self: center;
+  width: fit-content;
+  min-height: 100vh;
+  margin: 0 100px;
+}
+.wrapper {
+  margin: 0 -55px;
+  backdrop-filter: blur(40px);
 }
 </style>
