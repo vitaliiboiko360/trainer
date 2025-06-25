fec22dd31c2c3e775331ea1e1e52e0130b8e417d
vitalii boiko <vukelan@gmail.com>
2025-06-26 02:30:47 +0300

    update topbar height to store

--- src/page/topbar/TopBar.vue
+++ src/page/topbar/TopBar.vue
@@ -3,10 +3,25 @@ import BackHomeButton from './BackHomeButton.vue';
 import HiglightShowTranslationButton from './HiglightShowTranslationButton.vue';
 import TextDisplayButtonGroup from './TextViewButtonGroup.vue';
 import css from '../page.module.scss';
+
+import { ref, watchEffect } from 'vue';
+import { usePageHeightStore } from '../../store/pageHeight';
+
+const refDivRoot = ref();
+
+const pageHeightStore = usePageHeightStore();
+watchEffect(() => {
+  if (refDivRoot.value) {
+    pageHeightStore.updateTopBarHeight(refDivRoot.value.clientHeight);
+  }
+});
 </script>
 
 <template>
-  <div :class="[css.divTopBarOuter, $style.outerDiv]">
+  <div
+    :ref="(el) => (refDivRoot = el)"
+    :class="[css.divTopBarOuter, $style.outerDiv]"
+  >
     <div :class="css.divTopBarButtonColumn1">
       <BackHomeButton />
     </div>
