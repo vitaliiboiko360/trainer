a162a3c680b9cc24ee3f04253b6c41cf082d9c0c
vitalii boiko <vukelan@gmail.com>
2025-07-01 19:09:41 +0300

    bring control to slideshow

--- src/home/banner/Slideshow.vue
+++ src/home/banner/Slideshow.vue
@@ -3,12 +3,17 @@ import { nextTick, onMounted, onUpdated, ref, watch } from 'vue';
 import Slide from './Slide.vue';
 import SlideCircles from './SlideCircles.vue';
 import Slide_2 from './Slide_2.vue';
+import Slide_3 from './Slide_3.vue';
 
 const show1 = ref(false);
+const show2 = ref(false);
+const show3 = ref(false);
 const activeSlide = ref(-1);
 const isUpdated = ref(false);
 const resetSlide = () => {
   show1.value = false;
+  show2.value = false;
+  show3.value = false;
 };
 const setActiveSlide = (index) => {
   nextTick();
@@ -27,6 +32,12 @@ watch([activeSlide, isUpdated], () => {
   if (activeSlide.value == 1) {
     show1.value = true;
   }
+  if (activeSlide.value == 2) {
+    show2.value = true;
+  }
+  if (activeSlide.value == 3) {
+    show3.value = true;
+  }
 });
 
 onUpdated(() => {
@@ -37,7 +48,8 @@ onUpdated(() => {
 </script>
 
 <template>
-  <Slide v-if="0" key="0" />
-  <Slide_2 v-if="show1" key="1" />
-  <SlideCircles :resetSlide :setActiveSlide />
+  <Slide v-if="show1" key="0" />
+  <Slide_2 v-if="show2" key="2" />
+  <Slide_3 v-if="show3" key="3" />
+  <SlideCircles :numberOfSlides="3" :resetSlide :setActiveSlide />
 </template>
