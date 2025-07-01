3bffb458803924a6fa0fba1207e592e096ae5103
vitalii boiko <vukelan@gmail.com>
2025-07-01 18:02:55 +0300

    add state for active slide; add second slide show

--- src/home/banner/Slideshow.vue
+++ src/home/banner/Slideshow.vue
@@ -1,18 +1,43 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { nextTick, onMounted, onUpdated, ref, watch } from 'vue';
 import Slide from './Slide.vue';
 import SlideCircles from './SlideCircles.vue';
+import Slide_2 from './Slide_2.vue';
 
 const show1 = ref(false);
+const activeSlide = ref(-1);
+const isUpdated = ref(false);
+const resetSlide = () => {
+  show1.value = false;
+};
+const setActiveSlide = (index) => {
+  nextTick();
+  activeSlide.value = index;
+};
 
 onMounted(() => {
   setTimeout(() => {
     show1.value = true;
   }, 1500);
 });
+
+watch([activeSlide, isUpdated], () => {
+  if (activeSlide.value == -1) return;
+
+  if (activeSlide.value == 1) {
+    show1.value = true;
+  }
+});
+
+onUpdated(() => {
+  if (show1.value == false) {
+    isUpdated.value = !isUpdated.value;
+  }
+});
 </script>
 
 <template>
-  <Slide v-if="show1" />
-  <SlideCircles />
+  <Slide v-if="0" key="0" />
+  <Slide_2 v-if="show1" key="1" />
+  <SlideCircles :resetSlide :setActiveSlide />
 </template>
